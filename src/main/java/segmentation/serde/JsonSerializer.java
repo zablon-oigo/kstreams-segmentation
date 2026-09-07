e0d05323d974d9e2eccf324b3ce877060968ca3c
zablon <zablonoigo51@gmail.com>
2026-09-07 16:30:53 +0300

    json serializer

---
+++ src/main/java/segmentation/serde/JsonSerializer.java
@@ -0,0 +1,19 @@
+package segmentation.serde;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class JsonSerializer<T> implements Serializer<T> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public byte[] serialize(String topic, T data) {
+        if (data == null) return null;
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing to JSON for topic " + topic, e);
+        }
+    }
+}
